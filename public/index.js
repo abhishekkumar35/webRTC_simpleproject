0f6c02097056749958431b6ad6477beaa403ab35
Abhishek Kumar <flashtoak@gmail.com>
2023-08-19 00:43:58 +0530

    commit2

--- public/index.js
+++ public/index.js
@@ -4,7 +4,11 @@ const socket = io();
 const accountIdEl = document.getElementById("accountId");
 
 const remoteEl = document.getElementById("remoteAudioStreamDivId");
+const remoteAudioEl = createElement(remoteEl)
+remoteEl.appendChild(remoteAudioEl)
 const localEl = document.getElementById("localAudioStreamDivId");
+const localAudioEl = createElement("audio")
+localEl.appendChild(localAudioEl)
 
 const remoteSocketIdEl = document.getElementById("remoteId");
 const startCallEl = document.getElementById("localAudioInitButtonId");
@@ -25,8 +29,12 @@ startCallEl.addEventListener("click",async () => {
   if (remoteSocketIdEl.value === "") return;
 let stream = await navigator.mediaDevices.getUserMedia(constraints)
   localPeerConnection = new RTCPeerConnection(configuration);
+localAudioEl.srcObject = stream
+localPeerConnection.ontrack  = (e)=>{remoteAudioEl.src = e.streams[0]}
+stream.getTracks().forEach((track)=>{localPeerConnection8.addTrack(track)}):
 let sdpOffer = await localPeerConnection.createOffer()
 socket.emit("offer", sdpOffer)
+
 await localPeerConnection.setLocalDescription(sdpOffer)
 socket.on("answer",(answer)=>{localPeerConnection.setRemoteDescription(answer);})
 localPeerConnection.onicecandidate = ({candidate})={
@@ -35,3 +43,5 @@ socket.emit("candidate",candidate)
 }
 
 });
+
+
